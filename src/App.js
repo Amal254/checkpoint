ace6044fef9e505e706a5c685b20fb88acd4a6b3
Amal254 <amalbenammar8@gmail.com>
2023-02-18 09:41:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import UserList from './Components/UserList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Details from './Components/Details';
 
 function App() {
+  const [list, setList] = useState(null)
+  //console.log(list)
+  const [loading, setLoading] = useState(true)
+  const getUsers=()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setList(data)).then(setLoading(false)).catch(error=>console.log(error))
+  }
+  
+  useEffect(() => {
+    getUsers()
+
+  }, [])  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <UserList list={list} loading={loading}/>
+        }/>
+        <Route path="/detail/:id" element={<Details/>}/>
+      </Routes>
+    </Router>
+
     </div>
   );
 }
---
+++ src/Components/UserCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Button, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+const UserCard = ({user}) => {
+  return (
+    <div>
+    <Card style={{ width: '18rem' }}>
+    <Card.Body>
+      <Card.Title>{user.name}</Card.Title>
+      <Card.Text>
+      {user.email}
+      </Card.Text>
+      <Link to={`/detail/${user.id}`}> 
+      <Button variant="primary">info</Button>
+      </Link>
+    </Card.Body>
+  </Card>
+    </div>
+  )
+}
+
+export default UserCard
